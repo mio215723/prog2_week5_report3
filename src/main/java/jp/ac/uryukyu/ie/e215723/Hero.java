cac1ef1a9319672c300488def1630cd925db517b
Mio Miyara <e215723.ie.u-ryukyu.ac.jp>
2021-11-13 00:51:48 +0900

    Step4

--- src/main/java/jp/ac/uryukyu/ie/e215723/Hero.java
+++ src/main/java/jp/ac/uryukyu/ie/e215723/Hero.java
@@ -52,34 +52,66 @@ public class Hero {
         }
     }
 
+    /**
+     * 勇者の名前を教えてくれるメソッド。
+     * @return 勇者の名前
+     */
     public String getName(){
         return this.name;
     }
 
+    /**
+     * 勇者のHPを教えてくれるメソッド。
+     * @return 勇者のHP
+     */
     public int getHitPoint(){
         return this.hitPoint;
     }
 
+    /**
+     * 勇者の攻撃力を教えてくれるメソッド。
+     * @return 勇者の攻撃力
+     */
     public int getAttack(){
         return this.attack;
     }
     
+    /**
+     * 勇者が生きているか死んでいるか教えてくれるメソッド。
+     * @return　勇者が生きていればfalse、死んでいればtrue
+     */
     public boolean isDead(){
         return this.dead;
     }
 
+    /**
+     * 勇者の名前を変えるメソッド。
+     * @param name 新しい名前
+     */
     public void setName(String name){
         this.name = name;
     }
 
+    /**
+     * 勇者のHPを変えるメソッド。
+     * @param hitPoint 新しいHP
+     */
     public void setHitPoint(int hitPoint){
         this.hitPoint = hitPoint;
     }
 
+    /**
+     * 勇者の攻撃力を変えるメソッド。
+     * @param attack 新しい攻撃力
+     */
     public void setAttack(int attack){
         this.attack = attack;
     }
 
+    /**
+     * 勇者が生きているか死んでいるかを変えるメソッド。
+     * @param dead falseなら死んでいる。trueなら生きている。
+     */
     public void setDead(boolean dead){
         this.dead = dead;
     }
