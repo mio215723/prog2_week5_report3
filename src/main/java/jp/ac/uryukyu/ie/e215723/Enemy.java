61c0fac220768a413d5cdde12bfc19abb1a4b9af
Mio Miyara <e215723.ie.u-ryukyu.ac.jp>
2021-12-04 16:17:17 +0900

    REPORT4 Step1　変更追加

--- src/main/java/jp/ac/uryukyu/ie/e215723/Enemy.java
+++ src/main/java/jp/ac/uryukyu/ie/e215723/Enemy.java
@@ -1,12 +1,7 @@
 package jp.ac.uryukyu.ie.e215723;
 
 /**
- * 敵クラス。
- *  String name; //敵の名前
- *  int hitPoint; //敵のHP
- *  int attack; //敵の攻撃力
- *  boolean dead; //敵の生死状態。true=死亡。
- * Created by tnal on 2016/11/13.
+ * 敵クラス
  */
 public class Enemy extends LivingThing{
 
